2ab53890686b23ebfad43965b3ee8e65ad5fb249
Garret Akpale <garretakpale@gmail.com>
2024-02-11 05:14:20 -0800

    flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ This starts a Flash Web Application """
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -12,4 +13,3 @@ def hello_hbnb():
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
-
